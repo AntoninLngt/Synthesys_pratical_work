14d3f305cb0accef70b4f0c87989d7572f929fc6
EstelleTournassat <estelle.tournassat@ensea.fr>
2023-12-06 13:50:18 +0100

    question3 question4 upgrade

--- question3.c
+++ question3.c
@@ -9,48 +9,49 @@
 int main() {
     char user_input[MAX_INPUT_SIZE];
     const char *message_Welcome = "Welcome to ENSEA Tiny Shell. \nType 'exit' to quit. \n";
-    size_t message_Welcome_length = strlen(message_Welcome); //To mesure the lenght of the message for the write function
+    size_t message_Welcome_length = strlen(message_Welcome);
     write(STDOUT_FILENO, message_Welcome, message_Welcome_length);
 
     while (1) {
-        write(STDOUT_FILENO, "enseash % ", strlen ("enseash % "));
-        fgets(user_input, sizeof(user_input), stdin); // To save the anwers from the user
-        user_input[strcspn(user_input, "\n")] = '\0'; // To suppress the caracter \n
+        write(STDOUT_FILENO, "enseash % ", strlen("enseash % "));
         
-        
+        ssize_t bytesRead = read(STDIN_FILENO, user_input, sizeof(user_input));
+        if (bytesRead == -1) {
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+
+        if (bytesRead == 0) {
+            const char *message_Exit = "Bye bye...\n";
+            size_t message_Exit_length = strlen(message_Exit);
+            write(STDOUT_FILENO, message_Exit, message_Exit_length);
+            break;
+        }
+
+        user_input[bytesRead - 1] = '\0'; // Remove the newline character
+
         if (strcmp(user_input, "exit") == 0) {
             const char *message_Exit = "Bye bye...\n";
-            size_t message_Exit_length = strlen(message_Exit); 
+            size_t message_Exit_length = strlen(message_Exit);
             write(STDOUT_FILENO, message_Exit, message_Exit_length);
-            break;  // Leave the while loop to be able to quit the shell
-        }
-
-        else if ((getchar()) == EOF) {
-            const char *message_Exit = "Bye bye...\n";
-            size_t message_Exit_length = strlen(message_Exit); 
-            write(STDOUT_FILENO, message_Exit, message_Exit_length);
-            break;  // Leave the while loop to be able to quit the shell
-        }
-
-        else if (strcmp(user_input, "") == 0){
-        }
-        else {
-                pid_t pid = fork();
-                if (pid == -1) { // If error in the creation in the son process
+            break;
+        } else if (strcmp(user_input, "") == 0) {
+            // Do nothing for an empty input
+        } else {
+            pid_t pid = fork();
+            if (pid == -1) {
                 perror("fork");
                 exit(EXIT_FAILURE);
-                } 
-                else if (pid == 0) {
-                    execl("/bin/sh", "sh", "-c", user_input, NULL);
-            
-                    perror("execl"); // if there is an error
-                    exit(EXIT_FAILURE);
-                } 
-                else {
-                    waitpid(pid, NULL, 0);
-                }
+            } else if (pid == 0) {
+                execl("/bin/sh", "sh", "-c", user_input, NULL);
+                perror("execl");
+                exit(EXIT_FAILURE);
+            } else {
+                waitpid(pid, NULL, 0);
             }
-            putchar(end);
+            putchar('\n'); // Print a newline character after each command
         }
+    }
+
     return 0;
 }
