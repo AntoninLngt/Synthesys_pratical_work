3d9a9f7d16d12183eca395e9e1ae6cff6d1a21db
MrCaillou <antonin.longeot@gmail.com>
2023-12-13 17:23:36 +0100

    q3

--- TP2/put/puttftp_q3.c
+++ TP2/put/puttftp_q3.c
@@ -6,57 +6,65 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
-int main(int argc, char *argv[ ]) {
-    //getting the arguments host and file
+int main(int argc, char *argv[]) {
+    // Getting the arguments host and file
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <host> <file>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    char *host;
-    char *file;
-    if (argc==3){
-        host = argv[1];
-        file = argv[2];
-        printf("host = %s\n",host);
-        printf("file = %s\n",file);
-    }
 
+    char *host = argv[1];
+    char *file = argv[2];
+    printf("host = %s\n", host);
+    printf("file = %s\n", file);
 
     int s;
     struct addrinfo hints;
-    memset(&hints,0,sizeof(hints));
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;  // Specify UDP
     struct addrinfo *res;
 
-    // use of getnameinfo to get the name and adress of the host
-    s = getaddrinfo(host,NULL, &hints, &res);
+    // Use getaddrinfo to get address information
+    s = getaddrinfo(host, NULL, &hints, &res);
 
-    //manage the error if there are no arguments or if the arguments are false
+    // Manage errors if there are no arguments or if the arguments are false
     if (s != 0) {
-        printf("getaddrinfo: %s\n", gai_strerror(s));
-        exit(EXIT_FAILURE);
-    }
-    // make a connection to the good server with socket and connect
-    int sockfd = socket(res->ai_family,res-> ai_socktype, res-> ai_protocol);
-    connect(sockfd,res->ai_addr,res->ai_addrlen);
-
-    //errors management
-    if (sockfd == -1) {
-        perror("Erreur lors de la création du socket");
-        freeaddrinfo(res);
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
         exit(EXIT_FAILURE);
     }
 
-    // connection to the server
-    if (connect(sockfd, res->ai_addr, res->ai_addrlen) == -1) {
-        perror("Erreur lors de la connexion au serveur");
+    // Iterate through the results and create a socket for each result
+    struct addrinfo *p;
+    for (p = res; p != NULL; p = p->ai_next) {
+        int sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (sockfd == -1) {
+            perror("Error creating socket");
+            continue;  // Try the next result
+        }
+
+        // Print information about the address
+        char ipstr[INET6_ADDRSTRLEN];
+        void *addr;
+        if (p->ai_family == AF_INET) {
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+        } else {
+            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+        }
+        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        printf("Address: %s\n", ipstr);
+        printf("Protocol: %i (17 = UDP)\n", p->ai_protocol);
+        
+
+        // Close the socket
         close(sockfd);
-        freeaddrinfo(res);
-        exit(EXIT_FAILURE);
     }
 
-    // Close the socket and we free the memory
-    close(sockfd);
+    // Free the memory
     freeaddrinfo(res);
 
     return EXIT_SUCCESS;
